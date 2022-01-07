f730e323af6e48a7a4f397391840df5147bc477b
jp <69607974+letsDoTacos@users.noreply.github.com>
2022-01-06 20:51:11 -0600

    stopwatch w/ hooks and conditional rendering

---
+++ tutorials/stopwatch/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [time, setTime] = React.useState(0);
+  const [timerOn, setTimeOn] = React.useState(false);
+
+  React.useEffect(() => {
+    let interval = null;
+
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10)
+      }, 10)
+    } else {
+      clearInterval(interval)
+    }
+    return () => clearInterval(interval);
+  }, [timerOn])
+
+  return (
+    <div className="App">
+      <h1>Stopwatch</h1>
+        <div>
+          <span>{ ('0' + Math.floor(time / 60000) % 60).slice(-2) }:</span>
+          <span>{ ('0' + Math.floor(time / 1000) % 60).slice(-2) }:</span>
+          <span>{ ('0' + (time / 10) % 100).slice(-2) }</span>
+        </div>
+        <div>
+          { !timerOn && (
+            <button onClick={ () => setTimeOn(true) }>Start</button>
+
+          ) }
+          { timerOn && (
+            <button onClick={ () => setTimeOn(false) }>Stop</button>
+
+          ) }
+          { !timerOn && (
+          <button onClick={ () => setTimeOn(true) }>Resume</button>
+
+          ) }
+          { !timerOn && (
+          <button onClick={ () => setTime(0) }>Reset</button>
+          ) }
+        </div>
+    </div>
+  );
+}
+
+export default App;
